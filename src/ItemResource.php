961855494e29c36f7a23ec2cd43999abfd583626
Praesidiarius <admin@1plc.ch>
2022-11-10 05:39:35 +0100

    code cleanup

--- src/ItemResource.php
+++ src/ItemResource.php
@@ -2,9 +2,7 @@
 namespace Item;
 
 use Laminas\ApiTools\ApiProblem\ApiProblem;
-use Laminas\ApiTools\ContentNegotiation\ViewModel;
 use Laminas\ApiTools\Rest\AbstractResourceListener;
-use Laminas\Stdlib\Parameters;
 use stdClass;
 
 class ItemResource extends AbstractResourceListener
@@ -14,49 +12,24 @@ class ItemResource extends AbstractResourceListener
         private readonly ItemTableGateway $itemTbl
     )
     {
-
     }
 
-    /**
-     * Create a resource
-     *
-     * @param  mixed $data
-     * @return ApiProblem|mixed
-     */
-    public function create($data)
+    public function create($data) : ApiProblem
     {
         return new ApiProblem(405, 'The POST method has not been defined');
     }
 
-    /**
-     * Delete a resource
-     *
-     * @param  mixed $id
-     * @return ApiProblem|mixed
-     */
-    public function delete($id)
+    public function delete($id) : ApiProblem
     {
         return new ApiProblem(405, 'The DELETE method has not been defined for individual resources');
     }
 
-    /**
-     * Delete a collection, or members of a collection
-     *
-     * @param  mixed $data
-     * @return ApiProblem|mixed
-     */
-    public function deleteList($data)
+    public function deleteList($data) : ApiProblem
     {
         return new ApiProblem(405, 'The DELETE method has not been defined for collections');
     }
 
-    /**
-     * Fetch a resource
-     *
-     * @param  mixed $id
-     * @return ApiProblem|mixed
-     */
-    public function fetch($id)
+    public function fetch($id) : ApiProblem
     {
         return new ApiProblem(405, 'The GET method has not been defined for individual resources');
     }
@@ -76,48 +49,22 @@ class ItemResource extends AbstractResourceListener
         ];
     }
 
-    /**
-     * Patch (partial in-place update) a resource
-     *
-     * @param  mixed $id
-     * @param  mixed $data
-     * @return ApiProblem|mixed
-     */
-    public function patch($id, $data)
+    public function patch($id, $data) : ApiProblem
     {
         return new ApiProblem(405, 'The PATCH method has not been defined for individual resources');
     }
 
-    /**
-     * Patch (partial in-place update) a collection or members of a collection
-     *
-     * @param  mixed $data
-     * @return ApiProblem|mixed
-     */
-    public function patchList($data)
+    public function patchList($data) : ApiProblem
     {
         return new ApiProblem(405, 'The PATCH method has not been defined for collections');
     }
 
-    /**
-     * Replace a collection or members of a collection
-     *
-     * @param  mixed $data
-     * @return ApiProblem|mixed
-     */
-    public function replaceList($data)
+    public function replaceList($data) : ApiProblem
     {
         return new ApiProblem(405, 'The PUT method has not been defined for collections');
     }
 
-    /**
-     * Update a resource
-     *
-     * @param  mixed $id
-     * @param  mixed $data
-     * @return ApiProblem|mixed
-     */
-    public function update($id, $data)
+    public function update($id, $data) : ApiProblem
     {
         return new ApiProblem(405, 'The PUT method has not been defined for individual resources');
     }
